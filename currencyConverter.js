eb167757b0d8ee4e52f44214bc0b14e5a654f8cc
Pierre Lavaux <pierre.lavaux@plx.fr>
2026-03-31 12:39:08 +0700

    Code modernization (testing Claude).

--- currencyConverter.js
+++ currencyConverter.js
@@ -4,138 +4,94 @@ const path = require('path')
 
 module.exports = {
 
-    settings: {
-      url: "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
-    },
+  settings: {
+    url: 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
+  },
 
-    baseCurrency: "EUR",
+  baseCurrency: 'EUR',
 
-    currenciesMap: {},
+  currenciesMap: {},
 
-    currenciesMetadata: [],
+  readJson: function () {
+    return JSON.parse(fs.readFileSync(path.resolve(__dirname, 'Currencies.json'), 'utf8'))
+  },
 
-    executeCallback: null,
+  removeNamespaces: function (xml) {
+    return xml.replace(/(<\/?)(\w+:)/g, '$1').replace(/xmlns(:\w+)?="[^"]*"/g, '').trim()
+  },
 
-    readJson: function () {
-      const data = fs.readFileSync(path.resolve(__dirname, 'Currencies.json'), 'utf8')
-      return JSON.parse(data)
-    },
+  parseXML: async function (xml) {
+    const result = await xml2js.parseStringPromise(this.removeNamespaces(xml))
+    const currencies = result.Envelope.Cube[0].Cube[0].Cube
+    this.createCurrenciesMap(currencies)
+  },
 
-    removeNamespaces: function (xml) {
-      const fixedXML = xml.replace(/(<\/?)(\w+:)/g, '$1')
-      return (fixedXML.replace(/xmlns(:\w+)?="[^"]*"/g, '')).trim()
-    },
-
-    parseXML: function (xml) {
-      const cleanXML = this.removeNamespaces(xml)
-      const parser = new xml2js.Parser()
-
-      parser.parseString(cleanXML, (err, result) => {
-        const currencies = result.Envelope.Cube[0].Cube[0].Cube
-        this.createCurrenciesMap(currencies)
-      })
-    },
-
-    createCurrenciesMap: function (currencies) {
-      this.currenciesMap = {} // Reset map
-      currencies.forEach((item) => {
-         const currency = item.$.currency
-         const rate = item.$.rate
-         this.currenciesMap[currency] = Number(rate)
-      })
-      this.currenciesMap['EUR'] = 1
-      this.executeCallback()
-    },
-
-    getExchangeRates: async function () {
-      try {
-        console.log('Fetching exchange rates...')
-        const response = await fetch(this.settings.url)
-        if (response.ok) {
-          const data = await response.text()
-          this.parseXML(data)
-        }
-      } catch (error) {
-        console.error('Error:', error)
-      }
-    },
-
-    roundValues: function (value, places) {
-        const multiplier = Math.pow(10, places)
-        return (Math.round(value * multiplier) / multiplier)
-    },
-
-    fetchRates: function (settings) {
-      const fromRate = this.currenciesMap[settings.fromCurrency]
-      const toRate = this.currenciesMap[settings.toCurrency]
-
-      const rates = {}
-      rates.fromCurrency = { currency: settings.fromCurrency, rate: fromRate }
-      rates.toCurrency = { currency: settings.toCurrency, rate: toRate }
-      rates.exchangeRate = (1 / fromRate) * toRate
-      return rates
-    },
-
-    getAllCurrencies: function (callback) {
-      this.getExchangeRates()
-      this.executeCallback = function () {
-          // Return as array of {currency, rate} for compatibility
-          const currenciesArray = Object.entries(this.currenciesMap).map(([currency, rate]) => ({ currency, rate }))
-          callback(currenciesArray)
-        }
-    },
-
-    getBaseCurrency: function (callback) {
-      this.executeCallback = function () {
-          callback({ currency: 'EUR' })
-        }()
-    },
-
-    convert: function (settings, callback) {
-      this.getExchangeRates()
-      this.executeCallback = function () {
-          const exchangedValue = {}
-
-          const rates = this.fetchRates(settings)
-          const accuracy = settings.accuracy ?? 4
-          exchangedValue.currency = rates.toCurrency.currency
-          exchangedValue.exchangeRate = this.roundValues(rates.exchangeRate, accuracy)
-          exchangedValue.amount = this.roundValues(settings.amount * rates.exchangeRate, accuracy)
-
-          callback(exchangedValue)
-        }
-    },
-
-    getExchangeRate: function (settings, callback) {
-      this.getExchangeRates()
-      this.executeCallback = function () {
-          const exchangedValue = {}
-
-          const rates = this.fetchRates(settings)
-          const accuracy = settings.accuracy ?? 4
-          exchangedValue.toCurrency = rates.toCurrency.currency
-          exchangedValue.fromCurrency = rates.fromCurrency.currency
-          exchangedValue.exchangeRate = this.roundValues(rates.exchangeRate, accuracy)
-
-          callback(exchangedValue)
-        }
-    },
-
-    getCurrenciesMetadata: function (callback) {
-      this.currenciesMetadata = this.readJson()
-      callback(this.currenciesMetadata)
-    },
-
-    getCurrencyMetadata: function (settings, callback) {
-      this.currenciesMetadata = this.readJson()
-
-      const getCurrency = (currency) => {
-        return this.currenciesMetadata.find((item) => {
-           return item.Code === currency
-        })
-      }
-
-      callback(getCurrency(settings.currency))
+  createCurrenciesMap: function (currencies) {
+    this.currenciesMap = {
+      ...Object.fromEntries(currencies.map(({ $ }) => [$.currency, Number($.rate)])),
+      EUR: 1
     }
+  },
+
+  getExchangeRates: async function () {
+    console.log('Fetching exchange rates...')
+    const response = await fetch(this.settings.url)
+    if (!response.ok) throw new Error(`HTTP ${response.status}`)
+    await this.parseXML(await response.text())
+  },
+
+  roundValues: function (value, places) {
+    const multiplier = 10 ** places
+    return Math.round(value * multiplier) / multiplier
+  },
+
+  fetchRates: function ({ fromCurrency, toCurrency }) {
+    const fromRate = this.currenciesMap[fromCurrency]
+    const toRate = this.currenciesMap[toCurrency]
+    return {
+      fromCurrency: { currency: fromCurrency, rate: fromRate },
+      toCurrency: { currency: toCurrency, rate: toRate },
+      exchangeRate: toRate / fromRate
+    }
+  },
+
+  getAllCurrencies: async function () {
+    await this.getExchangeRates()
+    return Object.entries(this.currenciesMap).map(([currency, rate]) => ({ currency, rate }))
+  },
+
+  getBaseCurrency: function () {
+    return { currency: this.baseCurrency }
+  },
+
+  convert: async function (settings) {
+    await this.getExchangeRates()
+    const rates = this.fetchRates(settings)
+    const accuracy = settings.accuracy ?? 4
+    return {
+      currency: rates.toCurrency.currency,
+      exchangeRate: this.roundValues(rates.exchangeRate, accuracy),
+      amount: this.roundValues(settings.amount * rates.exchangeRate, accuracy)
+    }
+  },
+
+  getExchangeRate: async function (settings) {
+    await this.getExchangeRates()
+    const rates = this.fetchRates(settings)
+    const accuracy = settings.accuracy ?? 4
+    return {
+      fromCurrency: rates.fromCurrency.currency,
+      toCurrency: rates.toCurrency.currency,
+      exchangeRate: this.roundValues(rates.exchangeRate, accuracy)
+    }
+  },
+
+  getCurrenciesMetadata: function () {
+    return this.readJson()
+  },
+
+  getCurrencyMetadata: function ({ currency }) {
+    return this.readJson().find(item => item.Code === currency)
+  }
 
 }
--- examples/test.js
+++ examples/test.js
@@ -1,13 +1,8 @@
 const currencyConverter = require('../currencyConverter.js')
 
-const settings = {}
-settings.currency = "EUR"
+console.log(JSON.stringify(currencyConverter.getCurrencyMetadata({ currency: 'EUR' })))
 
-currencyConverter.getCurrencyMetadata(settings, (data) => {
-  console.log(JSON.stringify(data))
-})
-
-currencyConverter.getAllCurrencies((data) => {
+currencyConverter.getAllCurrencies().then((data) => {
   const USDEUR = 1 / data.find(({ currency }) => currency === 'USD').rate
   console.log(`USDEUR = ${USDEUR}`)
 })
